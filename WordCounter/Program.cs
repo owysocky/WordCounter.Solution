90f998d3adb1518ea7b4518bfcc3c7488faba6fe
ow <volovykolya@gmail.com>
2019-02-15 13:39:05 -0800

    add Main() initial lines

---
+++ WordCounter/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace WordCounter
+{
+  public class Program
+  {
+    public static void Main()
+    {
+
+     Console.WriteLine("====================================");
+     Console.WriteLine("COUNT IT");
+     Console.WriteLine("Enter a word that you want to count: ");
+     string wordInput = Console.ReadLine();
+     Console.WriteLine("Enter a sentence that you want to check: ");
+     string sentenceInput = Console.ReadLine();
+     RepeatCounter countIt = new RepeatCounter();
+
+    
+
+    }
+  }
+}
