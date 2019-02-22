effcd0e65d8bd990ee763d4c7c5132cbb8d697ee
ow <volovykolya@gmail.com>
2019-02-22 09:02:31 -0800

    add all initial files for controllers

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,4 +1,5 @@
-using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace WordCounter
 {
@@ -6,31 +7,14 @@ namespace WordCounter
   {
     public static void Main()
     {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-     Console.WriteLine("====================================");
-     Console.WriteLine("COUNT IT");
-     Console.WriteLine("Enter a word that you want to count: ");
-     string wordInput = Console.ReadLine();
-     Console.WriteLine("Enter a sentence that you want to check: ");
-     string sentenceInput = Console.ReadLine();
-     RepeatCounter countIt = new RepeatCounter();
-
-     bool isEmpty = countIt.IsEmpty(wordInput, sentenceInput);
-     if (isEmpty == true)
-     {
-       Console.WriteLine("The word '{0}' is used {1} times.", wordInput, countIt.WordCount(wordInput, sentenceInput));
-     }
-     else
-     {
-       Console.WriteLine("I am sorry, you've missed a line!");
-     }
-
-     Console.WriteLine("Want to count again? [y/n]");
-     string answer = Console.ReadLine();
-     if(answer == "y" || answer == "Y")
-     {
-       Main();
-     }
+      host.Run();
     }
   }
 }
